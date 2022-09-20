083f6c9e135a2aeddc4475cf56127c4285cbe490
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-20 15:25:18 +0530

    Added: Repeat till win

--- src/game/snake_ladder/SnakeLadderGame.java
+++ src/game/snake_ladder/SnakeLadderGame.java
@@ -5,34 +5,40 @@ import java.util.Random;
 public class SnakeLadderGame {
 
     public static void main(String[] args) {
-        int userPos = 0;
+        int userPos = 0, count = 0;
         Random random = new Random();
-        
+
         System.out.println("Welcome to snake ladder game: ");
-        
+
         System.out.println("*****GAME STARTED*****");
         System.out.println("Starting position of user = " + userPos);
-        
-        int dieNum = 1 + random.nextInt(6);
-        System.out.println("Die number: " + dieNum);
-        
-        int checkOption = random.nextInt(3);
-        
-        switch(checkOption) {
-            case 0:
-                System.out.println("No Play!");
-                break;
-            case 1:
-                System.out.println("Ladder!");
-                userPos += dieNum;
-                break;
-            default:
-                System.out.println("Snake!");
-                userPos -= dieNum;
-                if (userPos < 0)
-                    userPos = 0;
+
+        while (userPos != 100) {
+
+            count++;
+            int dieNum = 1 + random.nextInt(6);
+            System.out.println("Die number: " + dieNum);
+
+            int checkOption = random.nextInt(3);
+
+            switch (checkOption) {
+                case 0:
+                    System.out.println("No Play!");
+                    break;
+                case 1:
+                    System.out.println("Ladder!");
+                    userPos += dieNum;
+                    break;
+                default:
+                    System.out.println("Snake!");
+                    userPos -= dieNum;
+                    if (userPos < 0) {
+                        userPos = 0;
+                    }
+            }
+            System.out.println("New position of user: " + userPos);
         }
-        System.out.println("New position of user: " + userPos);
+        System.out.println("User Wins by rolling a die " + count + " times!");
     }
-    
+
 }
