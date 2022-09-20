77db4c6c0d417f93dec42f0ed433435e2dca4d4c
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-20 14:27:35 +0530

    Added: SnakeLadder Program

---
+++ src/game/snake_ladder/SnakeLadderGame.java
@@ -0,0 +1,13 @@
+package game.snake_ladder;
+
+public class SnakeLadderGame {
+
+    public static void main(String[] args) {
+        int userPos = 0;
+        System.out.println("Welcome to snake ladder game: ");
+        
+        System.out.println("*****GAME STARTED*****");
+        System.out.println("Starting position of user = " + userPos);
+    }
+    
+}
