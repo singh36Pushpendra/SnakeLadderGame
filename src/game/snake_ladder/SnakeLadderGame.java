815391cf5ed8df943669148756fc5cfe6a910fe6
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-20 14:50:03 +0530

    Added: Die value

--- src/game/snake_ladder/SnakeLadderGame.java
+++ src/game/snake_ladder/SnakeLadderGame.java
@@ -1,13 +1,20 @@
 package game.snake_ladder;
 
+import java.util.Random;
+
 public class SnakeLadderGame {
 
     public static void main(String[] args) {
         int userPos = 0;
+        Random random = new Random();
+        
         System.out.println("Welcome to snake ladder game: ");
         
         System.out.println("*****GAME STARTED*****");
         System.out.println("Starting position of user = " + userPos);
+        
+        int dieNum = 1 + random.nextInt(6);
+        System.out.println("Die number: " + dieNum);
     }
     
 }
