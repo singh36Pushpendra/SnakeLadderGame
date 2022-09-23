2222050d2be5459f4da622a5743371e7850287b0
singh36Pushpendra <psthakur.1999@gmail.com>
2022-09-23 17:45:00 +0530

    Added: uc7 two player game

--- src/game/snake_ladder/SnakeLadderGame.java
+++ src/game/snake_ladder/SnakeLadderGame.java
@@ -4,44 +4,70 @@ import java.util.Random;
 
 public class SnakeLadderGame {
 
-    public static void main(String[] args) {
-        int userPos = 0, count = 0;
+    static int count;
+
+    static int play(int playerPos) {
+
+        ++count;
         Random random = new Random();
+        int dieNum = 1 + random.nextInt(6);
+        System.out.println("Die number: " + dieNum);
+
+        int checkOption = random.nextInt(3);
+
+        switch (checkOption) {
+            case 0:
+                System.out.println("No Play!");
+                break;
+            case 1:
+                System.out.println("Ladder!");
+                playerPos += dieNum;
+                if (playerPos > 100) {
+                    playerPos -= dieNum;
+                }
+                play(playerPos);
+                break;
+            default:
+                System.out.println("Snake!");
+                playerPos -= dieNum;
+                if (playerPos < 0) {
+                    playerPos = 0;
+                }
+        }
+
+        return playerPos;
+    }
+
+    public static void main(String[] args) {
+        int player1Pos = 0, player2Pos = 0, chPlayCount = 0;
 
         System.out.println("Welcome to snake ladder game: ");
 
         System.out.println("*****GAME STARTED*****");
-        System.out.println("Starting position of user = " + userPos);
+        System.out.println("Starting position of player 1 = " + player1Pos);
+        System.out.println("Starting position of player 2 = " + player2Pos);
 
-        while (userPos != 100) {
-
-            count++;
-            int dieNum = 1 + random.nextInt(6);
-            System.out.println("Die number: " + dieNum);
-
-            int checkOption = random.nextInt(3);
-
-            switch (checkOption) {
-                case 0:
-                    System.out.println("No Play!");
-                    break;
-                case 1:
-                    System.out.println("Ladder!");
-                    userPos += dieNum;
-                    if (userPos > 100)
-                        userPos -= dieNum;
-                    break;
-                default:
-                    System.out.println("Snake!");
-                    userPos -= dieNum;
-                    if (userPos < 0) {
-                        userPos = 0;
-                    }
+        while (player1Pos != 100 && player2Pos != 100) {
+            chPlayCount++;
+            if (chPlayCount % 2 != 0) {
+                System.out.println("Player 1 is playing...");
+                player1Pos = play(player1Pos);
+                System.out.println("New position of Player 1: " + player1Pos + "\n");
+            } else {
+                System.out.println("Player 2 is playing...");
+                player2Pos = play(player2Pos);
+                System.out.println("New position of Player 2: " + player2Pos + "\n");
             }
-            System.out.println("User played " + count + " times till now!");
-            System.out.println("New position of user: " + userPos + "\n");
         }
+        if (player1Pos == 100) {
+            System.out.println("Player 1 won the game!");
+        } else {
+            System.out.println("Player 2 won the game!");
+        }
+        
+        
         System.out.println("Total " + count + " times dice was played to win!");
+
     }
 
 }
